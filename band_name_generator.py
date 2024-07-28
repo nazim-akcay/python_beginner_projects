cd2195648d3c13b8151d62986d68021cff794ee3
Nazim Akcay <145602373+nazim-akcay@users.noreply.github.com>
2024-07-28 12:44:47 +0300

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 23:59:20 2024
+
+@author: nazim
+"""
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+city_name = input("Please enter the name of the city you grew up in ?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("Please enter the name of your pet ?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Band name suggestion: " + city_name + " " + pet_name)
+
+
